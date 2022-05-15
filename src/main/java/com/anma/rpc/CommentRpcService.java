3f94ca92540192acc3b9087189415e0fbf55d20c
andrii <quadr988@gmail.com>
2022-05-15 13:55:36 +0300

    grpc

--- src/main/java/com/anma/rpc/CommentRpcService.java
+++ src/main/java/com/anma/rpc/CommentRpcService.java
@@ -1,69 +1,63 @@
-//package com.anma.rpc;
-//
-//import com.anma.Comment;
-//import com.anma.proto.CommentRequest;
-//import com.anma.proto.CommentResponse;
-//import com.anma.proto.CommentService;
-//import com.anma.repo.CommentRepo;
-//import io.quarkus.grpc.GrpcService;
-//import io.smallrye.mutiny.Multi;
-//import io.smallrye.mutiny.Uni;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import javax.inject.Inject;
-//import java.io.ByteArrayOutputStream;
-//import java.io.ObjectInputStream;
-//import java.io.ObjectOutputStream;
-//
-//@GrpcService
-//public class CommentRpcService implements CommentService {
-//
-//    private final Logger LOG = LoggerFactory.getLogger(CommentRpcService.class);
-//    @Inject
-//    CommentRepo commentRepo;
-//
-//    @Override
-//    public Uni<CommentResponse> getComment(CommentRequest request) {
-//
-//        System.out.println(request.getId());
-//
-//        var comment = commentRepo.findById(request.getId());
-//        LOG.info(">>> getting comment");
-//        LOG.info(comment.toString());
-//
-////        try {
-////            Thread.sleep(1000);
-////        } catch (InterruptedException e) {
-////            throw new RuntimeException(e);
-////        }
-//
-////        try {
-////            var baos = new ByteArrayOutputStream();
-////            var oos = new ObjectOutputStream(baos);
-////            oos.writeObject(comment);
-////            oos.flush();
-////            byte[] bytes = baos.toByteArray();
-////        } catch (Exception e) {
-////            e.printStackTrace();
-////        }
-//
-//
-////        Uni.createFrom().item();
-//
-//        return Uni.createFrom().item(() ->
-//                CommentResponse.newBuilder()
-//                        .setId(comment.onItem().transform(Comment::getId).await().indefinitely())
-//                        .setBody(comment.onItem().transform(c -> c.getBody()).await().indefinitely())
-//                        .setParentId(comment.onItem().transform(c -> c.getParentId()).await().indefinitely())
-//                        .build()
-//        );
-//    }
-//
-//    @Override
-//    public Multi<CommentResponse> streamComments(CommentRequest request) {
-////        return Multi.createFrom().generator();
-//        return null;    // todo
-//    }
-//}
-//
+package com.anma.rpc;
+
+import com.anma.Comment;
+import com.anma.proto.CommentRequest;
+import com.anma.proto.CommentResponse;
+import com.anma.proto.CommentService;
+import com.anma.repo.CommentRepo;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Optional;
+
+@GrpcService
+public class CommentRpcService implements CommentService {
+
+    private final Logger LOG = LoggerFactory.getLogger(CommentRpcService.class);
+    @Inject
+    CommentRepo commentRepo;
+
+    @Override
+    public Uni<CommentResponse> getComment(CommentRequest request) {
+
+        System.out.println(request.getId());
+
+        var comment = commentRepo.findById(request.getId());
+        LOG.info(">>> getting comment");
+        LOG.info(comment.toString());
+
+//        try {
+//            var baos = new ByteArrayOutputStream();
+//            var oos = new ObjectOutputStream(baos);
+//            oos.writeObject(comment);
+//            oos.flush();
+//            byte[] bytes = baos.toByteArray();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        return comment.onItem().ifNotNull().transform(c ->
+                CommentResponse.newBuilder()
+                        .setId(c.getId())
+//                        .setParentId(c.getParentId())
+                        .setBody(c.getBody())
+                        .build())
+                .onItem().ifNull().failWith(() -> new Exception("Error"))
+                .onFailure().invoke(fail -> System.out.println(fail));
+
+    }
+
+    @Override
+    public Multi<CommentResponse> streamComments(CommentRequest request) {
+
+        return Multi.createFrom().item(CommentResponse.newBuilder().setBody("aaa").build());    // todo
+    }
+}
+
--- src/main/java/com/anma/rpc/CommentRpcServiceBase.java
+++ src/main/java/com/anma/rpc/CommentRpcServiceBase.java
@@ -1,32 +1,33 @@
-package com.anma.rpc;
-
-import com.anma.proto.CommentRequest;
-import com.anma.proto.CommentResponse;
-import com.anma.proto.CommentServiceGrpc;
-import com.anma.repo.CommentRepo;
-import io.grpc.stub.StreamObserver;
-import io.quarkus.grpc.GrpcService;
-
-import javax.inject.Inject;
-
-@GrpcService
-public class CommentRpcServiceBase extends CommentServiceGrpc.CommentServiceImplBase {
-
-    @Inject
-    CommentRepo commentRepo;
-
-    @Override
-    public void getComment(CommentRequest request, StreamObserver<CommentResponse> responseObserver) {
-
-        var comment = commentRepo.findById(request.getId()).await().indefinitely();
-
-        responseObserver.onNext(CommentResponse.newBuilder()
-                .setId(comment.getId())
-                .setBody(comment.getBody())
-                .setParentId(comment.getParentId())
-                .build()
-        );
-        responseObserver.onCompleted();
-    }
-}
-
+//package com.anma.rpc;
+//
+//import com.anma.proto.CommentRequest;
+//import com.anma.proto.CommentResponse;
+//import com.anma.proto.CommentServiceGrpc;
+//import com.anma.repo.CommentRepo;
+//import io.grpc.stub.StreamObserver;
+//import io.quarkus.grpc.GrpcService;
+//
+//import javax.inject.Inject;
+//import java.time.Duration;
+//
+//@GrpcService
+//public class CommentRpcServiceBase extends CommentServiceGrpc.CommentServiceImplBase {
+//
+//    @Inject
+//    CommentRepo commentRepo;
+//
+//    @Override
+//    public void getComment(CommentRequest request, StreamObserver<CommentResponse> responseObserver) {
+//
+//        var comment = commentRepo.findById(request.getId()).await().atMost(Duration.ofSeconds(3));
+//
+//        responseObserver.onNext(CommentResponse.newBuilder()
+//                .setId(comment.getId())
+//                .setBody(comment.getBody())
+//                .setParentId(comment.getParentId())
+//                .build()
+//        );
+//        responseObserver.onCompleted();
+//    }
+//}
+//
